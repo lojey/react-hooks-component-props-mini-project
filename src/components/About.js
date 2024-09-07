04474262d59743cf9a1deefa25c7d9d911bc4e85
Gloria Jeyech <gjeyech@gmail.com>
2024-09-07 09:40:08 +0300

    update

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function About ({image = "https://via.placeholder.com/215"})
+{
+return (
+
+    <aside>
+        <img src={image} alt="blog logo"/>
+        <p>{About}</p>
+    </aside>
+
+
+);
+}
+
+export default About
