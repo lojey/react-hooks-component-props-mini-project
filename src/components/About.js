01fabf46dd9ecb29d8200c739efec14c41944ed6
Gloria Jeyech <gjeyech@gmail.com>
2024-09-07 10:15:41 +0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 
-function About ({image = "https://via.placeholder.com/215"})
-{
+function About ({image = "https://via.placeholder.com/215", about}) {
 return (
 
     <aside>
         <img src={image} alt="blog logo"/>
-        <p>{About}</p>
+        <p>{about}</p>
     </aside>
 
 
